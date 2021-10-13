957b60561a4e6ba75b39ea29fe27e55bc99828e1
Imad Arain <arain.imad@gmail.com>
2021-10-13 08:54:44 -0400

    Updating bug-bounty page

--- src/pages/bug-bounty.tsx
+++ src/pages/bug-bounty.tsx
@@ -14,11 +14,11 @@ export default function BugBountyPage() {
         <div className="md:flex md:justify-center">
           <div className="mx-4 md:text-lg max-w-screen-md">
             <article>
-              <h2 className="font-normal text-center pt-24 md:text-5xl">
+              <h2 className="font-normal text-center pt-24 md:text-5xl pb-6">
                 {intl.formatMessage({ id: 'bug-bounty-page-title' })}
               </h2>
-              <div className="py-12 md:flex">
-                <div className="bg-white md:mx-4 pt-5 pb-2 px-8 shadow-md ring-1 ring-black ring-opacity-5 h-full md:w-1/2">
+              {/* <div className="py-12 md:flex"> */}
+              {/* <div className="bg-white md:mx-4 pt-5 pb-2 px-8 shadow-md ring-1 ring-black ring-opacity-5 h-full md:w-1/2">
                   <a className="no-underline" href="/bug-bounty/#hunters">
                     <h3 className="font-normal text-textDark text-2xl hover:text-parityPink hover:underline">
                       {intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-title' })}
@@ -71,8 +71,8 @@ export default function BugBountyPage() {
                       </li>
                     </a>
                   </ul>
-                </div>
-                <div className="bg-white md:mx-4 my-6 md:my-0 pt-5 pb-2 px-8 shadow-md ring-1 ring-black ring-opacity-5 h-full md:w-1/2">
+                </div> */}
+              {/* <div className="bg-white md:mx-4 my-6 md:my-0 pt-5 pb-2 px-8 shadow-md ring-1 ring-black ring-opacity-5 h-full md:w-1/2">
                   <a className="no-underline" href="/bug-bounty/#contributors">
                     <h3 className="font-normal text-textDark text-2xl hover:text-parityPink hover:underline">
                       {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-title' })}
@@ -140,174 +140,142 @@ export default function BugBountyPage() {
                       </li>
                     </a>
                   </ul>
-                </div>
-              </div>
+                </div> */}
+              {/* </div> */}
 
-              <h3 id="hunters">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-title' })}</h3>
-              <h4 id="hunters-description">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-one' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-description-html' }),
-                }}
-              />
-              <h4 id="hunters-investigation-reporting">
-                {intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-two' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-responsible-investigation-html' }),
-                }}
-              />
-              <h4 id="hunters-eligibility">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-three' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-eligibility-html' }),
-                }}
-              />
-              <h4 id="hunters-ineligibility">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-four' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-ineligibility-html' }),
-                }}
-              />
-              <h4 id="hunters-reward">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-five' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-reward-html' }),
-                }}
-              />
-              <h4 id="hunters-report-bug">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-six' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-report-a-bug-html' }),
-                }}
-              />
-              <h4 id="hunters-legal-information">
-                {intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-seven' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-legal-information-html' }),
-                }}
-              />
-              <h4 id="hunters-governing-law">{intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-eight' })}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-governing-law-html' }),
-                }}
-              />
-              <h4 id="hunters-customer-services">
-                {intl.formatMessage({ id: 'bug-bounty-page-hunters-bullet-nine' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-hunters-customer-service-html' }),
-                }}
-              />
-              <hr />
-              <br />
-              <h3 id="contributors">{intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-title' })}</h3>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-title-html' }),
-                }}
-              />
-              <h4 id="contributors-description">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-one' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-html' }),
-                }}
-              />
-              <h4 id="contributors-making-contributions">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-two' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-making-contributions-html' }),
-                }}
-              />
-              <h4 id="contributors-refunds">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-three' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-refunds-and-cancellations-html' }),
-                }}
-              />
-              <h4 id="contributors-confidentiality">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-four' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-confidentiality-and-publicity-html' }),
-                }}
-              />
-              <h4 id="contributors-data-protection">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-five' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-data-protection-html' }),
-                }}
-              />
-              <h4 id="contributors-liability">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-six' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-exclusion-html' }),
-                }}
-              />
-              <h4 id="contributors-waiver">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-seven' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-waiver-html' }),
-                }}
-              />
-              <h4 id="contributors-third-party">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-eight' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-third-party-html' }),
-                }}
-              />
-              <h4 id="contributors-severance">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-nine' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-severance-html' }),
-                }}
-              />
-              <h4 id="contributors-agreement">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-ten' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-entire-agreement-html' }),
-                }}
-              />
-              <h4 id="contributors-governing-law">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-eleven' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-governing-law-html' }),
-                }}
-              />
-              <h4 id="contributors-customer-services">
-                {intl.formatMessage({ id: 'bug-bounty-page-contributors-bullet-twelve' })}
-              </h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: intl.formatMessage({ id: 'bug-bounty-page-contributors-customer-services-html' }),
-                }}
-              />
+              <h3>Help Us Make Parity More Secure!</h3>
+              <p>
+                We work hard to make sure the systems we build are bug-free, but acknowledge that we might not catch
+                them all. We call on our community and all bug bounty hunters to help identify bugs in the protocols and
+                software. If you discover a bug, we appreciate your cooperation in responsibly investigating and
+                reporting it to us so that we can address it as soon as possible.{' '}
+              </p>
+              <p>
+                Our Parity Bug Bounty Program allows us to recognise and reward members of the Parity community for
+                helping us find and address significant bugs, in accordance with the terms of the Parity Bug Bounty
+                Program set out below.
+              </p>
+              <p>
+                We want to remind all hunters that Parity’s main projects are blockchain-related source code (located in
+                our Github repositories) and associated released binaries, and not websites or services in any form.
+                This is the reason for our Bug Bounty Program covering only the former, and not the latter.
+              </p>
+              <h3>What&apos;s In Scope? (Hint: Not Our Website!)</h3>
+              <p>
+                If you&apos;ve found a potential bug in Substrate, Polkadot, or associated build and deployment
+                infrastructure, then we want to hear from you! Please reach out to us within 48 hours of discovery.
+              </p>
+              <p>Parity welcomes vulnerability reports that demonstrate security flaws in:</p>
+              <ul className="pl-4">
+                <li>
+                  <p className="font-bold mb-1">Substrate</p>
+                  <ul className="pl-4 mb-1">
+                    <li>
+                      Any bugs which can be used to bring down or take control of Substrate clients without direct
+                      access to the machine, including bugs in Substrate pallets and Substrate primitives
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <p className="font-bold mb-1">Polkadot</p>
+                  <ul className="pl-4 mb-1">
+                    <li>
+                      <strong>Client:</strong> Any bugs which can be used to bring down or take control of Parity
+                      Polkadot client without direct access to the machine
+                    </li>
+                    <li>
+                      <strong>Runtimes:</strong> Any bugs that compromise the intended behavior of the various
+                      Parity-built blockchain runtimes (Kusama, Polkadot, etc)
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <strong>Parity releases pipeline:</strong> any bugs which could be used to enable an attacker to
+                  inject malicious code into our distributed binaries, or be used to halt Parity’s release process or
+                  add malicious/unintended functions to the released binaries
+                </li>
+                <li>
+                  <strong>Production infrastructure:</strong> publicly-available infrastructure Parity runs for
+                  production-grade networks (in contrast to testnets), especially parts which are critical for network’s
+                  well-being or safety of funds.
+                </li>
+                <li>
+                  <strong>Cryptography code:</strong> any bugs relating to cryptography, encryption, decryption, and
+                  signing of messages (this includes account creation and recovery).
+                </li>
+                <li>
+                  <strong>Client Application Security:</strong> bugs which can allow users to obtain privileges not
+                  intended for them.
+                </li>
+              </ul>
+              <p>
+                Please note that where the scope of this policy includes third-party code this should not be taken as an
+                indication that we are legally or otherwise responsible for that code, its security, quality or your
+                rights in respect of that code.
+              </p>
+
+              <h3>Exclusions — What&apos;s NOT in Scope</h3>
+              <p>
+                Did you find a bug in our open source blockchain code or related infrastructure? Great! Tell us about
+                it!
+              </p>
+              <p>Most other things are not in scope, though. Specifically:</p>
+              <ul className="pl-4">
+                <li>
+                  The Parity websites, including{' '}
+                  <a href="https://parity.io" target="_blank" rel="noreferrer">
+                    https://parity.io
+                  </a>{' '}
+                  and{' '}
+                  <a href="https://substrate.dev" target="_blank" rel="noreferrer">
+                    https://substrate.dev
+                  </a>{' '}
+                  (including subdomains), and any others we might make in the future, are not in scope.
+                </li>
+                <li>
+                  Bugs which have already been submitted by another user or are already known to the Parity team or have
+                  already been publicly disclosed
+                </li>
+                <li>
+                  Bugs in third-party tools and services we’re using (but we would be glad to connect you with the
+                  security team of the corresponding project)
+                </li>
+                <li>
+                  Parity Technologies’ development team, Parity Technology employees and any other person employed or
+                  providing services in any way to the company, directly or indirectly, are not eligible for rewards.
+                </li>
+                <li>Anything that contravenes the spirit or letter of this Program.</li>
+              </ul>
+
+              <h3>Be Nice and So Will We!</h3>
+              <p>Responsible investigation and reporting includes, but isn&apos;t limited to, the following:</p>
+              <ul className="pl-4">
+                <li>
+                  You should use your best effort not to access, modify, delete, or store user data or Parity’s data.
+                  Instead, use your own accounts or test accounts for security research purposes.
+                </li>
+                <li>
+                  Don’t defraud,harm, or violate the privacy of Parity Technologies Ltd or its users during your
+                  research; you should make a good faith effort to not interrupt or degrade our services.
+                </li>
+                <li>
+                  Don&apos;t target our physical security measures, or attempt to use social engineering, spam,
+                  distributed denial of service (DDOS) attacks, etc.
+                </li>
+                <li>Initially report the bug only to us and not to anyone else.</li>
+                <li>Keep the details of any suspected bug confidential.</li>
+                <li>
+                  After reporting a suspected bug, give us a reasonable amount of time to fix the bug before disclosing
+                  it to anyone else, and seek our approval before disclosing it to anyone else. An uncoordinated public
+                  disclosure may lead to your submission being disqualified from the Program.
+                </li>
+                <li>
+                  Please don’t make repeat submissions of low quality, rejected or automated vulnerability reports.In
+                  general, please investigate and report bugs in a way that makes a reasonable, good faith effort not to
+                  be disruptive or harmful to us or our users. Otherwise your actions might be interpreted as an attack
+                  rather than an effort to be helpful.
+                </li>
+              </ul>
             </article>
           </div>
         </div>
