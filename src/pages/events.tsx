f07b0e8f8544aed636c47a36094d9e0486c24d70
Imad Arain <arain.imad@gmail.com>
2021-11-19 19:30:43 +0100

    layout updates for cards

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -24,14 +24,14 @@ export default function EventsPage() {
           </div>
         </div>
       </section>
-      <section className="bg-parityGray px-6">
+      <section className="bg-parityGray px-4">
         <div className="container">
-          <div className="flex flex-wrap justify-center lg:justify-start">
+          <div className="flex flex-wrap">
             {events
               .filter(event => new Date(event.expiration_date).getTime() - new Date().getTime() > 0)
               .map((event: any, index) => {
                 return (
-                  <div key={index}>
+                  <div key={index} className="w-1/1 md:w-1/2 xl:w-1/3">
                     <EventCard
                       dateTime={event.dateTime}
                       location={event.location}
@@ -47,17 +47,17 @@ export default function EventsPage() {
           </div>
         </div>
       </section>
-      <section className="bg-parityGray px-6">
+      <section className="bg-parityGray px-4">
         <div className="container">
           <div className="text-center py-8 md:py-16 md:flex md:flex-col md:justify-center">
             <h3 className="text-textDark text-center font-normal md:text-5xl mb-0 md:mb-8">Previous Events</h3>
           </div>
-          <div className="flex flex-wrap justify-center lg:justify-start">
+          <div className="flex flex-wrap">
             {events
               .filter(event => new Date(event.expiration_date).getTime() - new Date().getTime() < 0)
               .map((event: any, index) => {
                 return (
-                  <div key={index}>
+                  <div key={index} className="w-1/1 md:w-1/2 xl:w-1/3">
                     <EventCard
                       dateTime={event.dateTime}
                       location={event.location}
@@ -73,7 +73,7 @@ export default function EventsPage() {
           </div>
         </div>
       </section>
-      <section className="bg-parityGray">
+      <section className="bg-parityGray px-4">
         <div className="container">
           <div className="flex flex-col justify-center text-center py-16 md:py-24">
             <h3 className="font-normal mb-4 md:text-4xl">{intl.formatMessage({ id: 'contact-page-footer-title' })}</h3>
