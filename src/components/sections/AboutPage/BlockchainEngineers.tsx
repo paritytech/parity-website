0ef2276b1bae78cf3dfad81f7e8be2c405a5105e
Carina Robinson <carinalrobinson@gmail.com>
2022-04-05 17:38:09 +0200

    remove more facts section from about page

--- src/components/sections/AboutPage/BlockchainEngineers.tsx
+++ src/components/sections/AboutPage/BlockchainEngineers.tsx
@@ -35,11 +35,6 @@ export default function BlockchainEngineers() {
               </li>
               <li className="font-light pb-1">{intl.formatMessage({ id: 'about-page-team-stats-office-lisbon' })}</li>
             </ul>
-            <ul className="list-none text-sm">
-              <li className="font-medium pb-1">{intl.formatMessage({ id: 'about-page-team-stats-facts' })}</li>
-              <li className="font-light pb-1">{intl.formatMessage({ id: 'about-page-team-stats-facts-one' })}</li>
-              <li className="font-light pb-1">{intl.formatMessage({ id: 'about-page-team-stats-facts-two' })}</li>
-            </ul>
           </div>
         </div>
       </div>
