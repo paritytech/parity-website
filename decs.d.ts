1b7ed28d9853e7304765326c390a85ea0807fb5a
rmnprkrl <rmnprkrl@gmail.com>
2022-08-31 21:52:04 +0200

    remove ga trackers

--- decs.d.ts
+++ decs.d.ts
@@ -3,7 +3,6 @@ declare module '*.svg';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module 'gatsby-theme-i18n';
-declare module 'gatsby-plugin-gdpr-cookies';
 declare module 'mdx-utils';
 declare module 'prism-react-renderer/prism';
 declare module 'react-hubspot-form';
