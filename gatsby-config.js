5c228f9c8b870f85076fbb67c00c7824ec43ab16
carinaluise <carinalrobinson@gmail.com>
2023-01-06 13:20:55 +0100

    implement new sa endpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-simple-analytics',
       options: {
-        domain: 'api-sa.substrate.io',
+        domain: 'apisa.parity.io',
         eventsGlobal: 'sa',
         events: true,
         trackPageViews: true,
